e9da73377e181724013cac95d176566bcb859e67
Marcus Aldrey <marcusaldrey@gmail.com>
2021-11-01 14:16:16 -0300

    ⬆️ add pandas dependence

--- core/views.py
+++ core/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import redirect, render
+import pandas as pd
 
 # Create your views here.
 
 
 def home(request):
-    return redirect('upload')
+    return redirect("upload")
 
 
 def upload_file(request):
-    if request.method == 'GET':
-        return render(request, 'core/uploadfile.html')
-    elif request.method == 'POST':
+    if request.method == "GET":
+        return render(request, "core/uploadfile.html")
+    elif request.method == "POST":
         print(request.FILES)
-        return redirect('upload')
+        # get file path
+        file_path = request.FILES["file"].name
+        pd.read_excel(file_path, index_col=0)
+        return redirect("upload")
