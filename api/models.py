59168b7dcb93759ee8ac120a299f1c2e593a14ee
Marcus Aldrey <marcusaldrey@gmail.com>
2021-11-01 13:14:47 -0300

    🗃️ Create Person model and migration

---
+++ api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.db.models.fields import CharField, IntegerField
+
+# Create your models here.
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    class Gender(models.TextChoices):
+        MALE = 'M'
+        FEMALE = 'F'
+
+    gender = models.CharField(choices=Gender.choices, max_length=1)
+
+    height = models.IntegerField()
+    weight = models.FloatField()
+    birthdate = models.DateTimeField()
+    bairro = models.CharField(max_length=255)
+    estado = models.CharField(max_length=255)
+    numero = IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
